28df3112779d5640f154fc43fb159576da55035b
soporteghelo <soporte.ghelo@gmail.com>
2026-05-05 19:45:19 -0500

    fix: CORS error when sending data to Google Apps Script by using text/plain

--- src/App.tsx
+++ src/App.tsx
@@ -91,7 +91,7 @@ export default function App() {
         }));
         const response = await fetch(GOOGLE_SHEETS_WEBAPP_URL, {
           method: "POST",
-          headers: { "Content-Type": "application/json" },
+          headers: { "Content-Type": "text/plain;charset=utf-8" },
           body: JSON.stringify({ IdRef, participantes })
         });
         const result = await response.json();
